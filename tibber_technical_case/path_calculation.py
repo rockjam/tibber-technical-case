dc10248a4a8b2f33d5c3a039daf6a3da6fe7985d
Nikolai Tatarinov <5min4eq.unity@gmail.com>
2024-11-12 03:31:17 +0100

    faster path calculation, WIP

--- tibber_technical_case/path_calculation.py
+++ tibber_technical_case/path_calculation.py
@@ -1,32 +1,69 @@
 # returns a number of uniquely visited positions
 def count_unique_positions(x, y, commands):
-    positions = calculate_path(x, y, commands)
-    return len(set(positions))
+    path_segments = calculate_path(x, y, commands)
+    unique_positions_count = 0
+
+    # how many points on this segment are cleaned by other path
+    # only consider paths that are after this one
+    for i, this in enumerate(path_segments):
+        segment_unique_points = calculate_segment_points(this)
+        for j, other in enumerate(path_segments):
+            if i > j and perpendicular_cross(this, other):
+                segment_unique_points -= 1
+        unique_positions_count += segment_unique_points
+
+    return unique_positions_count
+
+
+# TODO: maybe it should return a number of intersections(0 or 1)
+def perpendicular_cross(seg1, seg2):
+    (x1, y1), (x2, y2) = seg1
+    (x3, y3), (x4, y4) = seg2
+
+    # seg1 horizontal, seg2 vertical
+    if y1 == y2 and x3 == x4:
+        return min(x1, x2) <= x3 <= max(x1, x2) and min(y3, y4) <= y1 <= max(y3, y4)
+
+    # seg1 vertical, seg2 horizontal
+    if x1 == x2 and y3 == y4:
+        return min(y1, y2) <= y3 <= max(y1, y2) and min(x3, x4) <= x1 <= max(x3, x4)
+
+    return False
+
+
+# calculates a number of points in the path segment
+def calculate_segment_points(segment):
+    (x1, y1), (x2, y2) = segment
+    if x1 == x2:
+        return abs(y2 - y1) + 1
+    elif y1 == y2:
+        return abs(x2 - x1) + 1
 
 
 # returns a list of all visited position, including positions visited several times
 def calculate_path(x, y, commands):
-    position_history = [(x, y)]
+    path_history = []
+    current_position = (x, y)
 
     for command in commands:
-        move(position_history, command)
+        new_position = move(current_position, command)
+        path_history.append((current_position, new_position))
+        current_position = new_position
 
-    return position_history
+    return path_history
 
 
-def move(position_history, command):
-    x, y = position_history[-1]
+def move(current_position, command):
     direction = command["direction"]
     steps = command["steps"]
 
-    for _ in range(steps):
-        match direction:
-            case "north":
-                y -= 1
-            case "east":
-                x += 1
-            case "south":
-                y += 1
-            case "west":
-                x -= 1
-        position_history.append((x, y))
+    x, y = current_position
+    match direction:
+        case "north":
+            return x, y - steps
+        case "east":
+            return x + steps, y
+        case "south":
+            return x, y + steps
+        case "west":
+            return x - steps, y
--- tibber_technical_case/test_path_calculation.py
+++ tibber_technical_case/test_path_calculation.py
@@ -1,4 +1,4 @@
-from tibber_technical_case.path_calculation import calculate_path, count_unique_positions
+from tibber_technical_case.path_calculation import calculate_path, count_unique_positions, perpendicular_cross
 
 easy_path = [
     {"direction": "east", "steps": 2},
@@ -22,7 +22,10 @@ back_forth_path = [
 
 
 def test_calculate_easy_path():
-    assert calculate_path(10, 22, easy_path) == [(10, 22), (11, 22), (12, 22), (12, 21)]
+    assert calculate_path(10, 22, easy_path) == [
+        ((10, 22), (12, 22)),
+        ((12, 22), (12, 21))
+    ]
 
 
 def test_count_easy_path():
@@ -30,7 +33,11 @@ def test_count_easy_path():
 
 
 def test_calculate_repeated_path():
-    assert calculate_path(10, 22, repeated_path) == [(10, 22), (11, 22), (12, 22), (12, 21), (12, 22), (12, 23)]
+    assert calculate_path(10, 22, repeated_path) == [
+        ((10, 22), (12, 22)),
+        ((12, 22), (12, 21)),
+        ((12, 21), (12, 23))
+    ]
 
 
 def test_count_repeated_path():
@@ -38,9 +45,48 @@ def test_count_repeated_path():
 
 
 def test_calculate_back_forth_path():
-    assert calculate_path(10, 22, back_forth_path) == [(10, 22), (10, 21), (10, 22),
-                                                       (10, 21), (10, 22), (10, 21), (10, 22)]
+    assert calculate_path(10, 22, back_forth_path) == [
+        ((10, 22), (10, 21)),
+        ((10, 21), (10, 22)),
+        ((10, 22), (10, 21)),
+        ((10, 21), (10, 22)),
+        ((10, 22), (10, 21)),
+        ((10, 21), (10, 22))
+    ]
 
 
 def test_count_back_forth_path():
     assert count_unique_positions(10, 22, back_forth_path) == 2
+
+
+def test_perpendicular_cross_intersection_middle():
+    assert perpendicular_cross(
+        ((1, 3), (3, 3)),
+        ((2, 2), (2, 4))
+    ) == True
+    assert perpendicular_cross(
+        ((2, 2), (2, 4)),
+        ((1, 3), (3, 3))
+    ) == True
+
+
+def test_perpendicular_cross_intersection_ends():
+    assert perpendicular_cross(
+        ((1, 1), (1, 3)),
+        ((1, 1), (3, 1))
+    ) == True
+    assert perpendicular_cross(
+        ((1, 1), (3, 1)),
+        ((1, 1), (1, 3))
+    ) == True
+
+
+def test_perpendicular_cross_no_intersection():
+    assert perpendicular_cross(
+        ((1, 1), (1, 3)),
+        ((2, 1), (3, 1))
+    ) == False
+    assert perpendicular_cross(
+        ((2, 1), (3, 1)),
+        ((1, 1), (1, 3))
+    ) == False
